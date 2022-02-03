d146d2292447e140ae06b8a8145bb53cd8b52ba9
Arthur Rossi Cardoso <arthurrossi15@gmail.com>
2022-02-03 13:10:22 -0300

    Filtros e categorias

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -4,7 +4,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -15,7 +14,6 @@ SECRET_KEY = ')81^#24268c$!dnoue@2+w^&hw@aa@&)=9_@p6v-7dc*b71a6i'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -60,7 +58,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'alurareceita.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -73,7 +70,6 @@ DATABASES = {
         'HOST': 'localhost'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -93,7 +89,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -107,7 +102,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
@@ -116,3 +110,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'alurareceita/static')
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- receitas/admin.py
+++ receitas/admin.py
@@ -4,11 +4,12 @@ from .models import Receita
 
 
 class ListandoReceitas(admin.ModelAdmin):
-    list_display = ('id', 'nome_receita', 'categoria', 'tempo_de_preparo', 'data_receita')
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_de_preparo', 'data_receita', 'publicada')
     list_display_links = ('id', 'nome_receita')
     search_fields = ('nome_receita',)
     list_filter = ('categoria',)
-    list_per_page = 3
+    list_editable = ('publicada',)
+    list_per_page = 5
 
 
 admin.site.register(Receita, ListandoReceitas)

49c369a5830b7dda68daaad97de0b128488b99bb
Arthur Rossi Cardoso <arthurrossi15@gmail.com>
2022-02-03 09:25:24 -0300

    Ajustando o Django admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import Receita
 
-admin.site.register(Receita)
+
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_de_preparo', 'data_receita')
+    list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita',)
+    list_filter = ('categoria',)
+    list_per_page = 3
+
+
+admin.site.register(Receita, ListandoReceitas)
